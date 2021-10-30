64f8e9d5cf359035242062388e878af034a5b53d
Miroslav Hrubeš <mira.hrubes@seznam.cz>
2021-10-30 20:21:43 +0200

    Komentare, drobne upravy a dokumentace

--- ukol_1.py
+++ ukol_1.py
@@ -1,14 +1,15 @@
 # DEADLINE: 6.11. v 23:59
 # Vše najdeš na webu
-from turtle import *
+from turtle import * #Import všech funkcí ze želvy
 
-print("Zadej počet sloupců")
+print("Zadejte počet sloupců hracího pole.")  #Volitelná velikost hrací plochy
 x = int(input())
-print("Zadej počet řádků")
+print("Zadejte počet řádků hracího pole.")
 y = int(input())
-for _ in range (x):   
+for _ in range (x):   #Vykreslení šestiúhelníkové hrací plochy
     for _ in range(y):  
-        for _ in range(6):
+        for _ in range(6): #Vykreslení jednoho šestiúhelníku
+            speed(10)
             forward(30) 
             left(60)
         left(120)
@@ -17,7 +18,7 @@ for _ in range (x):
         forward(30) 
         right(60) 
     forward(30)
-    for _ in range(y):
+    for _ in range(y): #Návrat želvy z vrchního šestiúhelníku na bod, odkud se bude vykreslovat další sloupec.
         right(60)
         forward(30)
         right(60)
@@ -26,8 +27,8 @@ for _ in range (x):
     left(60)
     forward(30)
     right(60)    
-penup()
-goto(0,0)
+penup() #Želva při pohybu nebude psát
+goto(0,0) #Přesun želvy na počáteční souřadnice
 
 
 print("Souřadnice zadávejte následujícím způsobem:")
@@ -35,43 +36,43 @@ print("číslo sloupce -> enter -> číslo řádku -> enter")
 pocpol = y*x
 
 hrac1 = True
-while pocpol > 0 :
-    if hrac1 == True:
+while pocpol > 0 : #Cyklus se bude opakovat, dokud nebudou všechna pole plná
+    if hrac1 == True: #Střídání hráčů
         hraccis = 1
     else:
         hraccis = 2   
     print("Hráči",hraccis,"zadej souřadnice políčka.")
     sx = int(input())
     sy = int(input())
-    while sx > x or sy > y:
+    while sx > x or sy > y: #Kontrola správnosti zadaných souřadnic
         print("Tato souřadnice je mimo pole hry. Zadejte souřadnice znovu.")
         sx = int(input())
         sy = int(input())
     
-    for _ in range (sx-1):
+    for _ in range (sx-1): #Posun nekreslící želvy na správný sloupec
         forward(30)
         left(60)
         forward(30)
         right(60)
-    for _ in range (sy-1):
+    for _ in range (sy-1): #Posun nekreslící želvy na správný řádek
         left(120)
         forward(30)
         right(60)
         forward(30)
         right(60)
-    left(60)
+    left(60) #Posun nekreslící želvy do středu šestiúhleníku
     forward(30)
     right(15)
     if hrac1 == True:
         pendown()
-        for _ in range (4):
+        for _ in range (4): #Vykreslení křížku
             forward(15)
             left(180)
             forward(15)
             left(90)
         right(45)
         
-    else:
+    else: #Vykreslení kolečka
         right(135)
         forward(15)
         left(90)
@@ -80,8 +81,8 @@ while pocpol > 0 :
         
     penup()
     goto(0,0)    
-    pocpol -= 1
-    hrac1 = not hrac1
+    pocpol -= 1 #Odečtení jednoho volného pole, na které byla právě nakreslena značka
+    hrac1 = not hrac1 #Výměna hráčů
 print("Konec hry!")   
 
 
@@ -94,4 +95,4 @@ print("Konec hry!")
 
 
 
-exitonclick()
+exitonclick() #Nechá otevřené okno s želvou
